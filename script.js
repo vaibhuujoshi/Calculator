eef27aa23a34353893b6fe339453e3ff341457f9
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-01-01 19:35:43 +0530

    Feat: Calculator

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
-function add(num1,num2) {
+function add(num1, num2) {
     return num1 + num2;
 }
 
-function subtract(num1,num2) {
+function subtract(num1, num2) {
     return num1 - num2;
 }
 
-function multiply(num1,num2) {
+function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1,num2) {
+function divide(num1, num2) {
     return num1 / num2;
 }
 
-function operator(oprt,num1, num2) {
+function CalculateOperation(oprt, num1, num2) {
     if (oprt === "+") {
-        add(num1, num2)
+        return add(num1, num2)
     } else if (oprt === "-") {
-        subtract(num1,num2)
+        return subtract(num1, num2)
     } else if (oprt === "*") {
-        multiply(num1, num2)
+        return multiply(num1, num2)
     } else if (oprt === "/") {
-        divide(num1, num2)
+        return divide(num1, num2)
     }
 }
 
-let btns = document.querySelector("button");
+let allowedOperators = ["+", "-", "*", "/"]
 
-btns.addEventListener("click", (item) => {
-    return alert(btns.textContent)
-} );
+let allowedValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/"]
+
+let btns = document.querySelectorAll("button");
+
+const display = document.querySelector("#display");
+
+btns.forEach((btn) => {
+    if (allowedValue.includes(btn.textContent)) {
+        btn.addEventListener("click", () => display.textContent += btn.textContent)
+    }
+
+    else if (btn.textContent === "clr") {
+        btn.addEventListener("click", () => display.textContent = "")
+
+    }
+
+    else if (btn.textContent === "=") {
+        btn.addEventListener("click", () => {
+            let operationData = display.textContent
+            let splitedOperation = operationData.split("");
+
+            let operator;
+
+            for (let i of splitedOperation) {
+                if (allowedOperators.includes(i)) {
+                    operator = i
+                }
+            }
+
+            let operationValues = operationData.split(operator);
+
+            let oprt = operator;
+            // alert(oprt)
+            let num1 = parseInt(operationValues[0]);
+            let num2 = parseInt(operationValues[1]);
+
+            let result = CalculateOperation(oprt, num1, num2);
+
+            display.textContent = result;
+
+        })
+    }
+})
